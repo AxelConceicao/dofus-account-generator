fd52b7ee3b8d85369c37a62848136023722924c6
Axel Conceicao <axel.conceicao@epitech.eu>
2020-04-18 04:30:05 +0200

    proxy scraper done

--- creator.py
+++ creator.py
@@ -1,50 +1,81 @@
 import sys
 import time
+import json
 from lxml import html
 import requests
+from requests.exceptions import Timeout
+import urllib.request
+import socket
+import urllib.error
 
-class Ressource:
-    id = None
-    name = None
-    img = None
-    def __init__(self, id, name, img):
-        self.id = id
-        self.name = name
-        self.img = img
+def is_bad_proxy(pip):    
+    try:
+        proxy_handler = urllib.request.ProxyHandler({'http': pip})
+        opener = urllib.request.build_opener(proxy_handler)
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        urllib.request.install_opener(opener)
+        req=urllib.request.Request('http://www.example.com')  # change the URL to test here
+        sock=urllib.request.urlopen(req, timeout=5)
+    except urllib.error.HTTPError as e:
+        print('Error code: ', e.code)
+        return e.code
+    except Exception as detail:
+        print("ERROR:", detail)
+        return True
+    return False
 
-    def write(self, file):
-        file.writerow([str(self.id), self.name])
-
-    def saveImg(self):
-        img_data = requests.get(self.img).content
-        with open("img/" + str(self.id) + '.png', 'wb') as handler:
-            handler.write(img_data)
-
-    def dump(self):
-        print(str(self.id) + ':' + self.name)
-
-class Scraper:
-    start = None
+class Creator:
     file = None
 
-    def __init__(self, file, start = 1):
+    def __init__(self, file):
         self.file = file
-        self.start = start
-        self.scrap()
+
+    def checkProxy(self, ip, port):
+        url = 'https://api.github.com/'
+        proxies = {
+            "http": 'http://' + ip + ':' + port,
+            "https": 'https://' + ip + ':' + port,
+        }
+        try:
+            response = requests.get(url, proxies=proxies, timeout=5)
+        except Timeout:
+            print('The request timed out')
+            return None
+        except requests.exceptions.ConnectionError:
+            print("Connection refused")
+            return None
+        return response.status_code
+        
+    def getLocalProxyList(self):
+        proxies = []
+        with open('proxies.txt', 'r') as file:
+            proxies = file.readlines()
+        for proxy in proxies:
+            ip, port = proxy.strip('\n').split(':')
+            print(self.checkProxy(ip, port))
+    
+    def getOnlineProxy(self):
+        url = 'http://pubproxy.com/api/proxy?api=cDhCQVlKaGlTWXNlRXpLMmxYOHZDZz09&type=https'
+        response = requests.get(url)
+        currentProxy = json.loads(response.text)['data'][0]['ipPort']
+        print('Trying ' + currentProxy)
+        if is_bad_proxy(currentProxy):
+            print("Bad Proxy %s" % (currentProxy))
+        else:
+            print("%s is working" % (currentProxy))
+
+
+    def run(self):
+        while True:
+            self.getOnlineProxy()
+        # self.getLocalProxyList()
+
+        
 
 if __name__ == "__main__":
     filename = 'accounts.txt'
-    start = None
-    if (len(sys.argv) > 1):
-        start = int(sys.argv[1])
-    data = []
-    with open('items.csv', 'r') as csvfile:
-        r = csv.reader(csvfile, encoding='utf-8')
-        for row in r:
-            data.append(row)
-    with open('items.csv', 'w') as csvfile:
-        w = csv.writer(csvfile, delimiter=',')
-        for row in data:
-            w.writerow(row)
-        Scraper(w, start)
+    if (len(sys.argv) == 2):
+        filename = sys.argv[1]
+    Creator(filename).run()
+    # with open(filename, 'a') as w:
     exit(0)
