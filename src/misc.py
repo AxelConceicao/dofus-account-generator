830eeb231b38eca3ac60e8a4ad2c49d8acf37b07
Axel Conceicao <axel.conceicao@epitech.eu>
2020-04-21 00:16:53 +0200

    Add useful functions like one that generates random alphanumeric string for username and password

---
+++ src/misc.py
@@ -0,0 +1,20 @@
+import sys
+import json
+import string
+import random
+import colorama
+from colorama import Fore, Style
+
+def ePrint(msg):
+    print(Fore.RED + Style.BRIGHT + 'Error: ' + msg + Style.RESET_ALL)
+    
+def sPrint(msg):
+    print(Fore.GREEN + Style.BRIGHT + 'Success: ' + msg + Style.RESET_ALL)
+
+def random_alphaNumeric_string(lettersCount, digitsCount):
+    sampleStr = ''.join((random.choice(string.ascii_letters) for i in range(lettersCount)))
+    sampleStr += ''.join((random.choice(string.digits) for i in range(digitsCount)))
+    sampleList = list(sampleStr)
+    random.shuffle(sampleList)
+    finalString = ''.join(sampleList)
+    return finalString
